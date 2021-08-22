b5c4f1961c3a9e2a398d2009b82932e1cb876bc0
Steven Woodard <swoodard4@outlook.com>
2021-08-21 20:48:30 -0400

    Upload files

---
+++ Tutorial 7-1 SWOODARD/Lottery Numbers/Lottery Numbers/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace Lottery_Numbers
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.firstLabel = new System.Windows.Forms.Label();
+            this.secondLabel = new System.Windows.Forms.Label();
+            this.thirdLabel = new System.Windows.Forms.Label();
+            this.fourthLabel = new System.Windows.Forms.Label();
+            this.fifthLabel = new System.Windows.Forms.Label();
+            this.generateButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.sumTotalLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // firstLabel
+            // 
+            this.firstLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.firstLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.firstLabel.Location = new System.Drawing.Point(14, 9);
+            this.firstLabel.Name = "firstLabel";
+            this.firstLabel.Size = new System.Drawing.Size(42, 23);
+            this.firstLabel.TabIndex = 0;
+            this.firstLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // secondLabel
+            // 
+            this.secondLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.secondLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.secondLabel.Location = new System.Drawing.Point(62, 9);
+            this.secondLabel.Name = "secondLabel";
+            this.secondLabel.Size = new System.Drawing.Size(42, 23);
+            this.secondLabel.TabIndex = 1;
+            this.secondLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // thirdLabel
+            // 
+            this.thirdLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.thirdLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.thirdLabel.Location = new System.Drawing.Point(110, 9);
+            this.thirdLabel.Name = "thirdLabel";
+            this.thirdLabel.Size = new System.Drawing.Size(42, 23);
+            this.thirdLabel.TabIndex = 2;
+            this.thirdLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // fourthLabel
+            // 
+            this.fourthLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.fourthLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fourthLabel.Location = new System.Drawing.Point(158, 9);
+            this.fourthLabel.Name = "fourthLabel";
+            this.fourthLabel.Size = new System.Drawing.Size(42, 23);
+            this.fourthLabel.TabIndex = 3;
+            this.fourthLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // fifthLabel
+            // 
+            this.fifthLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.fifthLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fifthLabel.Location = new System.Drawing.Point(206, 9);
+            this.fifthLabel.Name = "fifthLabel";
+            this.fifthLabel.Size = new System.Drawing.Size(42, 23);
+            this.fifthLabel.TabIndex = 4;
+            this.fifthLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // generateButton
+            // 
+            this.generateButton.Location = new System.Drawing.Point(53, 52);
+            this.generateButton.Name = "generateButton";
+            this.generateButton.Size = new System.Drawing.Size(75, 34);
+            this.generateButton.TabIndex = 5;
+            this.generateButton.Text = "Generate Numbers";
+            this.toolTip1.SetToolTip(this.generateButton, "Generate And Display Lottery Numbers");
+            this.generateButton.UseVisualStyleBackColor = true;
+            this.generateButton.Click += new System.EventHandler(this.generateButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(134, 52);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 34);
+            this.exitButton.TabIndex = 6;
+            this.exitButton.Text = "Exit";
+            this.toolTip1.SetToolTip(this.exitButton, "Close The Program");
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(216, 52);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(95, 20);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.Text = "Totaled Numbers:";
+            // 
+            // sumTotalLabel
+            // 
+            this.sumTotalLabel.Location = new System.Drawing.Point(211, 75);
+            this.sumTotalLabel.Name = "sumTotalLabel";
+            this.sumTotalLabel.Size = new System.Drawing.Size(100, 23);
+            this.sumTotalLabel.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(323, 107);
+            this.Controls.Add(this.sumTotalLabel);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.generateButton);
+            this.Controls.Add(this.fifthLabel);
+            this.Controls.Add(this.fourthLabel);
+            this.Controls.Add(this.thirdLabel);
+            this.Controls.Add(this.secondLabel);
+            this.Controls.Add(this.firstLabel);
+            this.Name = "Form1";
+            this.Text = "Lottery Numbers -SWoodard";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label firstLabel;
+        private System.Windows.Forms.Label secondLabel;
+        private System.Windows.Forms.Label thirdLabel;
+        private System.Windows.Forms.Label fourthLabel;
+        private System.Windows.Forms.Label fifthLabel;
+        private System.Windows.Forms.Button generateButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label sumTotalLabel;
+    }
+}
+
---
+++ Tutorial 7-1 SWOODARD/Lottery Numbers/Lottery Numbers/Form1.cs
@@ -0,0 +1,77 @@
+﻿//Author: Steven Woodard with help drawn from B. Clay Shannon's post with subsequent reviewers: https://stackoverflow.com/questions/12808943/how-can-i-get-all-labels-on-a-form-and-set-the-text-property-of-those-with-a-par
+//Also taken from Starting Out with Visual C# by Tony Gaddis.
+//Date: 11/2/2019
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Lottery_Numbers
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            //close the program
+            this.Close();
+        }
+        private void generateButton_Click(object sender, EventArgs e)
+        {
+            //declare number of lottery numbers as the same as the number of labels available
+            //find all of the controls marked as "labels" on the scope, which are not "sumTotalLabel" and make a 'label' array
+            Label[] labels = Controls.OfType<Label>().Where<Label>(eachLabel => eachLabel.Name != "sumTotalLabel").ToArray();
+            int numOfLotteryNums = labels.Length;
+            //create array for lottery numbers
+            int[] lotteryNumbers = CreateArrayForLotteryNumbers(numOfLotteryNums);
+            //store random numbers in array
+            PutRandomNumsInLottery(ref lotteryNumbers);
+            //Display the lottery numbers
+            DisplayLotteryNumbers(lotteryNumbers, labels);
+
+        }
+        private int[] CreateArrayForLotteryNumbers(int numOfLotteryNumbers = 5)
+        {
+            //return an array with the default number of numbers being 5.
+            return new int[numOfLotteryNumbers];
+        }
+        private void PutRandomNumsInLottery(ref int[] lotteryNumbers)
+        {
+            //accumulator
+            int myAccum = 0;
+            //take in a reference to the variable inside the external scope
+            //Create a random object for generating random numbers
+            Random rand = new Random();
+            //iterate through the array assigning the lottery numbers with the random object
+            for(int i = 0; i < lotteryNumbers.Length; i++)
+            {
+                lotteryNumbers[i] = rand.Next(100);
+                // display the total of the lottery numbers
+                myAccum += lotteryNumbers[i];
+            }
+            sumTotalLabel.Text = myAccum.ToString();
+
+        }
+        public void DisplayLotteryNumbers(int[] lotteryNumbers, Label[] labels)
+        {
+            //take in the int array and the Label array
+            
+            //iterate through the array and change the text of the labels to the lottery numbers
+            for(int i = 0; i < labels.Length; i++)
+            {
+                labels[i].Text = lotteryNumbers[i].ToString();
+            }
+            
+        }
+
+    }
+
+}
